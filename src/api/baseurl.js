a17998c0f5af9b039b3dadec885d6f89e5aee120
gnanaparthiban <gnanaparthiban@xtowns.in>
2025-12-24 09:26:20 +0530

    api changes

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,6 +1,6 @@
 // src/config.js
-// export const BASE_API = "https://gms-backend-vdwc.onrender.com/api/v1";
-export const BASE_API = "http://192.168.31.14:4000/api/v1";
+export const BASE_API = "https://gms-backend-vdwc.onrender.com/api/v1";
+// export const BASE_API = "http://192.168.31.14:4000/api/v1";
 
 
 export default BASE_API;
